474d33b1510849fbcc9aaa0bcc709847cf7cae51
shaheemMPM <shaheem.mpm@gmail.com>
2020-02-05 20:21:05 +0530

    refactor folder name and add simple boat v1.0.0 to server

---
+++ 3_Two_Way_Communication_with_Boat_on_server/Server.java
@@ -0,0 +1,90 @@
+// A Java program for a Server
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Server{
+	//initialize socket and input stream
+	private Socket		 socket = null;
+	private DataOutputStream out	 = null;
+	private ServerSocket server = null;
+	private DataInputStream in	 = null;
+  public static String client_name, user_name;
+
+	// constructor with port
+	public Server(int port){
+		// starts server and waits for a connection
+		try{
+			server = new ServerSocket(port);
+
+			System.out.println("Server started");
+			System.out.println("Waiting for a client ...");
+
+			socket = server.accept();
+
+			// takes input from the client socket
+			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+      // takes input from terminal
+			Scanner input = new Scanner(System.in);
+
+			// sends output to the socket
+			out = new DataOutputStream(socket.getOutputStream());
+
+			out.writeUTF(user_name);
+
+      client_name = in.readUTF();
+
+			System.out.println(client_name+"[client] connected");
+
+			String line = "";
+
+			// reads message from client until "Over" is sent
+			while (true){
+				try{
+					line = in.readUTF();
+					if (line.equals("Over")) {
+						System.out.println("Connection closed by Client!!!");
+						break;
+					}
+					System.out.println(client_name+" : "+line);
+          String response = respond(line);
+          if (response.equals("")) {
+            System.out.print("me : ");
+            line = input.nextLine();
+          }else{
+            line = "me : "+response;
+          }
+          out.writeUTF(line);
+					if (line.equals("Over")) {
+						System.out.println("Connection closing...");
+						break;
+					}
+				}catch(IOException i){
+					System.out.println(i);
+				}
+			}
+
+			// close connection
+			socket.close();
+			in.close();
+			input.close();
+		}catch(IOException i){
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String args[]){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter user name : ");
+    user_name = scanner.nextLine();
+		Server server = new Server(3000);
+	}
+
+  public static String respond(String query){
+    if (query.equals("What is your name?")) {
+      return "Mohammed Shaheem P";
+    }
+    return "";
+  }
+}
